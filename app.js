70e558598b714ecb983d0ce124bbf416b8a92e83
Luis Rosado <luismrosado@yahoo.com>
2020-01-09 17:39:24 +0000

    js basics strings

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Hello World!');
+// console.log('Hello World!');
+// var language = 'JavaScript';
+// var status = 'Awesome';
+// var ourThoughts = language + ' is ' + status;
+
+// console.log(ourThoughts);	
+
+var greeting = 'Hello';
+greeting += ' World!'; 
+
+console.log('greeting:', greeting);
+
+var capitalGreet = greeting.toUpperCase();
+var lowerCaseGreet = greeting.toLowerCase();
+
+console.log('capitalGreet:', capitalGreet);
+console.log('lowerCaseGreet:', lowerCaseGreet);
