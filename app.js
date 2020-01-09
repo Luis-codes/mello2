0349eed5bb9fc766fb5066d65e07b0d65c531fa9
Luis Rosado <luismrosado@yahoo.com>
2020-01-09 18:55:08 +0000

    js basics: strings, numbers, arrays

--- app.js
+++ app.js
@@ -5,13 +5,76 @@
 
 // console.log(ourThoughts);	
 
-var greeting = 'Hello';
-greeting += ' World!'; 
+// var greeting = 'Hello';
+// greeting += ' World!'; 
 
-console.log('greeting:', greeting);
+// console.log('greeting:', greeting);
 
-var capitalGreet = greeting.toUpperCase();
-var lowerCaseGreet = greeting.toLowerCase();
+// var capitalGreet = greeting.toUpperCase();
+// var lowerCaseGreet = greeting.toLowerCase();
 
-console.log('capitalGreet:', capitalGreet);
-console.log('lowerCaseGreet:', lowerCaseGreet);
+// console.log('capitalGreet:', capitalGreet);
+// console.log('lowerCaseGreet:', lowerCaseGreet);
+
+// var sentence = 'the quick brown fox jumped over the lazy dog';
+// var words = sentence.split(' ');
+// console.log(words);
+
+// var firstName = 'Brendan';
+// var lastName = 'Eich';
+
+// console.log('firstName:', firstName);
+// console.log('lastName:', lastName);
+
+// var fullName = `
+// 	Full Name
+
+// 	--- 
+
+// 	First Name: ${firstName}
+// 	Last Name: ${lastName}`;
+
+// 	console.log(fullName);
+
+// var price = 23.09;
+// var tax = 0.07
+// var total = price * (1 + tax);
+
+// console.log(total);
+
+// var age = 11;
+// age++;
+// console.log(age);
+// age--;
+// console.log(age);
+
+// Undefined is implicit emptiness
+// noVal is never given a value, so it is undefined
+// var noVal;
+// console.log(noVal); // undefined
+
+// Null is explicit emptiness
+// Values must be assigned null
+// var nullVal = null;
+// console.log(nullVal); // null
+
+var favoriteThings = ['dogs', 'nature', 'friends'];
+var firstThing = favoriteThings[0];
+var secondThing = favoriteThings[1];
+var thirdThing = favoriteThings[2];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
